f6d03d035d6161c30702c936ce47dac73ea7407f
Bubumariia Kudaiberdieva <mariiakudaiberdieva@gmail.com>
2022-10-13 12:52:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { reviews } from "./components/data";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: reviews,
+      currentReview: 0,
+    };
+  }
+  handleNextBtn = () => {
+    if (this.state.currentReview >= this.state.data.length - 1) {
+      this.setState({ currentReview: 0 });
+    } else {
+      this.setState({ currentReview: this.state.currentReview + 1 });
+    }
+  };
+  handlePrevBtn = () => {
+    if (this.state.currentReview <= 0) {
+      this.setState({ currentReview: this.state.data.length - 1 });
+    } else {
+      this.setState({ currentReview: this.state.currentReview - 1 });
+    }
+  };
+  handleSurpriseBtn = () => {
+    let randomNumber = Math.floor(Math.random() * this.state.data.length);
+    this.setState({ currentReview: randomNumber });
+  };
+  render() {
+    const { data, currentReview } = this.state;
+    const access = data[currentReview];
+    console.log(data);
+    return (
+      <React.Fragment>
+        <div className="container">
+          <h1>Our reviews</h1>
+          <div className="reviews">
+            <img className="image" src={access.img} alt="image" />
+            <h3 className="name">{access.name}</h3>
+            <h4 className="job">{access.job}</h4>
+            <p className="text">{access.text}</p>
+            <div>
+              <button className="next-btn" onClick={this.handlePrevBtn}>
+                Previews
+              </button>
+              <button className="next-btn" onClick={this.handleNextBtn}>
+                Next
+              </button>
+            </div>
+
+            <button className="surprise-btn" onClick={this.handleSurpriseBtn}>
+              Surprise me
+            </button>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
